919b891bae53fb0a0c50f77c9010071f3ca1d679
Hamza Shakir <60213466+hamzashakir99@users.noreply.github.com>
2023-05-28 17:09:54 +0500

    remove js file

--- examples/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { SmartMessage } from '../../src/index'
-var root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(React.createElement(React.StrictMode, null, React.createElement(SmartMessage, { domain: '', token: '' })))
-//# sourceMappingURL=index.js.map
